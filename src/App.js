dd0b8d255a295000de677a492a7b72e8a43870a4
zacb <z.bennett@treasure.tech>
2025-02-17 12:41:25 -0800

    floating answer state

--- src/App.js
+++ src/App.js
@@ -3,12 +3,11 @@ import { questions } from "./data";
 
 const questionToAnswerSet = {};
 for (let quiz of questions) {
-    for (let question of quiz.questions) {
-        questionToAnswerSet[question.question] = question.correctAnswer
-    }
+  for (let question of quiz.questions) {
+    questionToAnswerSet[question.question] = question.correctAnswer;
+  }
 }
 function App() {
-  //   const originalAnswers =  {};
   const originalAnswers = localStorage.getItem("answerstate21025") || {};
   const [selectedAnswers, setSelectedAnswers] = useState(
     Object.keys(originalAnswers).length === 0 ? {} : JSON.parse(originalAnswers)
@@ -26,44 +25,68 @@ function App() {
   };
   return (
     <div className="App" style={{ maxWidth: "800px", margin: "0 auto" }}>
-      <h1>Quiz App</h1>
-      <button onClick={() => setSelectedAnswers({})}>Clear answers</button>
+      <h1>Project Heartbeat Quizzes</h1>
       {questions.map((section) => {
         return (
           <>
-            <h2 style={{textAlign: 'left', paddingBottom: '16px'}}>{section.title}</h2>
+            <h2 style={{ textAlign: "left", paddingBottom: "16px" }}>
+              {section.title}
+            </h2>
             <ol>
-            {section.questions.map((elem, index) => {
-                return <div key={index} style={{textAlign: 'left', paddingBottom: '16px'}}>
-                <li><strong>{elem.question}</strong></li>
-                <div style={{display: 'flex', flexDirection: 'column'}}>
-                  {Object.keys(elem.answers).map((key) => (
-                    <label key={key}>
-                      <input
-                        type="radio"
-                        name={`question_${elem.question}`}
-                        value={key}
-                        onChange={() => handleAnswerChange(elem.question, key)}
-                        checked={selectedAnswers[elem.question] === key}
-                      />
-                      {key}: {elem.answers[key]}
-                    </label>
-                  ))}
-                </div>
-                {selectedAnswers[elem.question] && (
-                  <p>
-                    {isCorrect(elem.question) ? 'Correct!' : 'Wrong!'}
-                  </p>
-                )}
-              </div>
-            })}
+              {section.questions.map((elem, index) => {
+                return (
+                  <div
+                    key={index}
+                    style={{ textAlign: "left", paddingBottom: "16px" }}
+                  >
+                    <li>
+                      <strong>{elem.question}</strong>
+                    </li>
+                    <div style={{ display: "flex", flexDirection: "column" }}>
+                      {Object.keys(elem.answers).map((key) => (
+                        <label key={key}>
+                          <input
+                            type="radio"
+                            name={`question_${elem.question}`}
+                            value={key}
+                            onChange={() =>
+                              handleAnswerChange(elem.question, key)
+                            }
+                            checked={selectedAnswers[elem.question] === key}
+                          />
+                          {key}: {elem.answers[key]}
+                        </label>
+                      ))}
+                    </div>
+                    {selectedAnswers[elem.question] && (
+                      <p>{isCorrect(elem.question) ? "Correct!" : "Wrong!"}</p>
+                    )}
+                  </div>
+                );
+              })}
             </ol>
           </>
         );
       })}
-      Correct:{" "}
-      {Object.keys(selectedAnswers).filter((key) => isCorrect(key)).length} /{" "}
-      {Object.keys(selectedAnswers).length}
+      <div
+        style={{
+          position: "fixed",
+          bottom: "5%",
+          right: "5%",
+          border: "solid 1px black",
+          borderRadius: "5px",
+          display: "flex",
+          flexDirection: "column",
+          rowGap: "16px",
+          padding: "8px",
+          backgroundColor: "white"
+        }}
+      >
+        Correct:{" "}
+        {Object.keys(selectedAnswers).filter((key) => isCorrect(key)).length} /{" "}
+        {Object.keys(selectedAnswers).length}
+        <button onClick={() => setSelectedAnswers({})}>Clear answers</button>
+      </div>
     </div>
   );
 }
